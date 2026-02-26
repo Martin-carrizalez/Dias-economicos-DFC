4055b7aed5ed4dd8840645f705cb74cbe58964a9
Martin Carrizalez <martin.carrizalez0823@alumnos.udg.mx>
2026-02-25 18:00:17 -0600

    lo mismo las nehritas

--- app3.py
+++ app3.py
@@ -721,7 +721,7 @@ def generar_comisiones_word(df_comisiones, tipo_comision, oficio_inicial, fecha_
                                     run.text = ''
                                 if paragraph.runs:
                                     paragraph.runs[0].text = texto_completo
-                                    paragraph.runs[0].bold = False  # Quitar negritas
+                                   
             docs.append(doc)
             oficio_actual += 1
         
